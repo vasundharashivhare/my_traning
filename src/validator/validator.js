1c3889a8661a2fd48a7c4f4b127883fe9847fbe6
sameerpanigrahy <100825520+sameerpanigrahy@users.noreply.github.com>
2022-09-11 00:30:08 +0530

    final code

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -1,11 +1,18 @@
-const isValidMail = (/^([0-9a-z]([-_\\.]*[0-9a-z]+)*)@([a-z]([-_\\.]*[a-z]+)*)[\\.]([a-z]{2,9})+$/);
+const isValidMail = (/^([0-9a-z]([-_\\.]*[0-9a-z]+)*)@([a-z]([-_\\.]*[a-z]+)*)[\\.]([a-z]{2,20})+$/);
 
-
-const isValidName = (/^[a-zA-Z ]*$/)
-
+const isValidPassword = function (value) {
+    if (  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,12}$/.test(value) ) return true;
+    return false;
+  };
+const isValidName = (/^[a-zA-Z. ]{3,20}$/)
 
 const isValid = (value) => {
     if (typeof value === "undefined" || value === null) return false
+    if (typeof value === "string" && value.trim().length === 0) return false
+    return true
+}
+
+const isValidfild = (value) => {
     if (typeof value === "string" && value.trim().length === 0) return false
     return true
 }
@@ -17,12 +24,7 @@ const isValidRequestBody = (value) => {
 
 
 
-
-
-
-
-
 module.exports = {
-    isValidMail, isValid, isValidName, isValidRequestBody
+    isValidMail, isValid, isValidName, isValidRequestBody,isValidPassword,isValidfild
 }
 

babb681638fa4929a9f4efad8845042b0547b0c2
sameerpanigrahy <100825520+sameerpanigrahy@users.noreply.github.com>
2022-09-11 00:27:59 +0530

    final code

--- src/Middleware/Middleware.js
+++ src/Middleware/Middleware.js
@@ -1,23 +1,24 @@
 const jwt = require("jsonwebtoken")
-const blogModel = require("../model/blogsModel")
 
 
 const authenticate = async function (req, res, next) {
-    try {
-      let token = req.headers["x-api-key"] 
-      if (!token) return res.status(400).send({ status: false, msg: "token must be present in the request header" })
-      req.token = token
-      let decodedToken = jwt.verify(token, 'thou-hath-the-poer')
-      if(!decodedToken)  return res.status(401).send({status:false,msg:"unauthanticated user"})
-      req.decodedToken = decodedToken
-      next()
-    }
-    catch (err) {
-      res.status(500).send({ msg: err })
-    }
+  try {
+    let token = req.headers["x-api-key"]
+    if (!token) return res.status(400).send({ status: false, msg: "token must be present in the request header" })
+
+    // validating the token
+    let decodedToken = jwt.verify(token, "thou-hath-the-poer")
+          req.decodedToken = decodedToken
+         next()
   }
+    catch (err) {
+      return res.status(500).send({ msg: err })
+     }
+}
 
- 
 
-  module.exports.authenticate=authenticate
-  
+
+
+
+
+module.exports.authenticate = authenticate

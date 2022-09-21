95decfc77490e3420e6f163fc34de2b9fdec54ca
PritalRupnur <pritalrupnur29012000@gmail.com>
2022-09-21 11:54:56 +0530

    breate book

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,4 +1,4 @@
-const bookModel=require('../models/bookModel')
+const bookModel=require('../models/booksModel')
 
 const createBook=async function(req,res){
     let requestBody=req.body
@@ -7,4 +7,11 @@ const createBook=async function(req,res){
     res.status(201).send({status:true,message:'Success',data:bookData})
 }
 
+
+const getBook=async function(req,res){
+    let requestBody=req.body
+    if(!requestBody) return res.status(400).send({status:false,message:'book data is required in body'})
+    let bookData=await bookModel.create(requestBody)
+    res.status(201).send({status:true,message:'Success',data:bookData})
+}
 module.exports.createBook=createBook
--- src/models/booksModel.js
+++ src/models/booksModel.js
@@ -4,13 +4,13 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 const bookSchema = new mongoose.Schema({
     title: { type: String, required: true, unique: true },
     excerpt: { type: String, required: true },
-    userId: { ObjectId, mandatory, ref: 'userDB' },
+    userId: { type:ObjectId, required: true, ref: 'userDB' },
     ISBN: { type: String, required: true, unique: true },
     category: { type: String, required: true },
-    subcategory: { type: [String], required: true },
-    reviews: { type: Number, default: 0, comment: 'Holds number of reviews of this book' },
+    subcategory: { type: String, required: true },
+    reviews: { type: Number, default: 0 },//comment: 'Holds number of reviews of this book' 
     deletedAt: { type: Date },
-    isDeleted: { type: boolean, default: false },
+    isDeleted: { type: Boolean, default: false },
     releasedAt: { type: Date, required: true } //, format("YYYY-MM-DD")
 
 }, { timestamps: true })

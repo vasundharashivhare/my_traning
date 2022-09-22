358a66bd02036ac17b6fa553feebcaa670f646f8
prince9871 <soniprince142@gmail.com>
2022-09-23 00:43:16 +0530

    reviews data check done

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -29,8 +29,8 @@ const createReview = async function (req, res) {
 
 const updatereview = async function (req, res) {
     try {
-        let bookId = req.params.bookId
-        let reviewId = req.params.reviewId
+        let params = req.params
+        let { bookId, reviewId } = params
         let queries = req.body
         let { reviewedBy, rating, review } = queries
         let filter = {}
@@ -39,13 +39,12 @@ const updatereview = async function (req, res) {
         if (rating) filter.rating = rating
         if (review) filter.review = review
 
-        if (bookId) { var checkId = await bookModel.findOne({_id:bookId, isDeleted:false}).lean() }
-        if (!bookId) return res.status(404).send({ status: false, message: "book Data not found" })
+        let checkId = await bookModel.findOne({ _id: bookId, isDeleted: false }).select({ __v: 0 }).lean()
+        if (!checkId) return res.status(404).send({ status: false, message: "book Data not found" })
         if (checkId) { var idOfReview = await reviewModel.findOneAndUpdate({ _id: reviewId, bookId: bookId, isDeleted: false }, filter, { new: true }) }
         if (!idOfReview) return res.status(404).send({ status: false, message: " Data not found" })
 
-        const reviews = await reviewModel.find({ bookId: bookId, isDeleted: false })
-
+        const reviews = await reviewModel.find({ bookId: bookId, isDeleted: false }).select({ __v: 0 })
         checkId.reviewsData = reviews
 
         return res.status(201).send({ status: true, message: "Updated Successfully", data: checkId })
@@ -56,23 +55,23 @@ const updatereview = async function (req, res) {
 }
 
 
-const deleteReview=async function(req,res){
-    try{
+const deleteReview = async function (req, res) {
+    try {
         let bookId = req.params.bookId
         let reviewId = req.params.reviewId
-      
-        if (bookId) { var checkId = await bookModel.findOne({_id:bookId, isDeleted:false}) }
+
+        if (bookId) { var checkId = await bookModel.findOne({ _id: bookId, isDeleted: false }) }
         if (!bookId) return res.status(404).send({ status: false, message: "book Data not found" })
-        if (checkId) { var idOfReview = await reviewModel.findOneAndUpdate({ _id: reviewId, bookId: bookId, isDeleted: false }, {isDeleted:true, }, { new: true }) }
+        if (checkId) { var idOfReview = await reviewModel.findOneAndUpdate({ _id: reviewId, bookId: bookId, isDeleted: false }, { isDeleted: true, }, { new: true }) }
         if (!idOfReview) return res.status(404).send({ status: false, message: " Data not found" })
-        if (idOfReview) { var reducedReviews = await bookModel.findOneAndUpdate({_id:bookId}, {$inc:{reviews:-1}}, {new:true}) }
-     
+        if (idOfReview)  await bookModel.findOneAndUpdate({ _id: bookId }, { $inc: { reviews: -1 } }, { new: true }) 
 
-        return res.status(200).send({ status: true, message: "Deleted Successfully"})
- 
-    }catch(err){
+        return res.status(200).send({ status: true, message: "Deleted Successfully" })
+    }
+     catch (err) {
         return res.status(500).send({ err: err.message })
-    }}
-    
+    }
+}
 
-    module.exports = { createReview, updatereview, deleteReview }
+
+module.exports = { createReview, updatereview, deleteReview }
--- src/validators/validation.js
+++ src/validators/validation.js
@@ -1,5 +1,54 @@
+const mongoose=require('mongoose')
+
 const isvalidISBN=function(isbn){
     return /^(97[8|9]-)(\d{10,13}+)/.test(isbn)
 }
 
-module.exports={isvalidISBN}
+const isValidTitle = function (title) {
+    return ['Mr', 'Mrs', 'Miss'].indexOf(title) !== -1
+}
+
+const isValidPassword = function (password) {
+    return (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/.test(password))
+}
+
+const isvalidPincode=function(pincode){
+    return /^\d{6}$/.test(pincode)
+}
+
+const isvalidRating=function(rating){
+    return /^[1-5]$/.test(rating)
+}
+
+const isValidMobileNumber=function(number){
+    return /^[6-9]\d{9}$/.test(number) //9587412693
+}
+
+const isValidString = function (value) {
+    let regex = /^[a-zA-Z]+([\s][a-zA-Z]+)*$/
+    return regex.test(value)//Prince Soni
+}
+
+const isValidEmail=function(email){
+    return /^\S+@\S+\.\S+$/.test(email) //abc@gmail.com
+}
+
+const isLowerCase=function(name){
+    return /^([a-z]+)$/.test(name) //atoz
+} 
+
+const isValidSpace = function (value) {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
+    return true;
+}
+
+const isvalidRequest=function(body){
+    return Object.keys(body).length > 0 //plz enter the data in the body
+}
+
+const isValidObjectId = function (objectId) {
+    return mongoose.Types.ObjectId.isValid(objectId) //24
+}
+
+module.exports={isvalidISBN,isValidTitle,isValidPassword,isvalidPincode,isvalidRating,isValidMobileNumber,isValidString,isValidEmail,isLowerCase,isValidSpace,isValidObjectId,isvalidRequest}
